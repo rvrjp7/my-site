522c888434a2d60217e7886a6250f7f7a71290f4
Raviraj Patil <rvrjp7@gmail.com>
2023-01-15 02:02:09 +0530

    add heart animation

--- src/components/Engagement/index.js
+++ src/components/Engagement/index.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import { MainContainer } from './styles'
+import Lottie from 'react-lottie';
+import animationData from './animation.json';
 const engagementEngImg = require('../../assets/engagement_english.jpg')
-
 const Engagement = () => {
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: animationData,
+        rendererSettings: {
+          preserveAspectRatio: "xMidYMid slice"
+        }
+      };
   return (
     <MainContainer className='engagement-english'>
         <img src={engagementEngImg} alt=""></img>
+        <Lottie 
+	    options={defaultOptions}
+        style={{position:"absolute"}}
+      />
     </MainContainer>
   )
 }
--- src/components/Engagement/styles.js
+++ src/components/Engagement/styles.js
@@ -5,9 +5,16 @@ export const MainContainer = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: space-evenly;
+    overflow: hidden;
+    position: relative;
     img{
         max-height: calc(100vh - 200px);
         object-fit: contain;
         width: 100%;
+    }
+    .anime{
+        position: absolute;
+        height: 100vh;
+        width: 100vw;
     }
 `
